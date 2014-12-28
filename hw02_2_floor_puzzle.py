b91295bd79b0fd6b5a249df2488988335fd92b4a
domspad <domspad@umich.edu>
2014-12-28 18:07:30 -0500

    did floor puzzle... not much to plan out since so similar... now checking solution

---
+++ hw02_2_floor_puzzle.py
@@ -0,0 +1,15 @@
+import itertools 
+
+def floor_puzzle() :
+    floors = range(1,6)
+    orderings = itertools.permutations(floors)
+    return next( (Hopper, Kay, Liskov, Perlis, Ritchie) for (Hopper, Kay, Liskov, Perlis, Ritchie) in orderings
+                                                        if Hopper != 5
+                                                        if Kay != 1
+                                                        if Liskov not in [1,5]
+                                                        if Perlis > Kay
+                                                        if abs(Ritchie - Liskov) != 1
+                                                        if abs(Liskov - Kay) != 1)
+
+if __name__ == '__main__' :
+	print(floor_puzzle())
