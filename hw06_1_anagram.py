9af0bc0b9bb9e405b3b887f5fe8c98143e93de3a
domspad <domspad@umich.edu>
2015-01-27 09:38:42 -0500

    started hw6 anagrams

---
+++ hw06_1_anagram.py
@@ -0,0 +1,17 @@
+
+
+# DICTIONARY = readwordlist
+# PREFIX_DICTIONARY = 
+'''
+Concept inventory:
+
+WORDS
+PREFIXES
+readwordlist
+find_words(letters, after='',shortest=2)
+	returns set of possible words from leters after 'THE', with min length
+
+
+
+
+'''
